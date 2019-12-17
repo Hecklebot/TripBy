b7d7d020d10cc655ac2c18fbf60999184ffded74
Hecklebot <51806574+Hecklebot@users.noreply.github.com>
2019-12-17 20:54:13 +0900

    jdbc.properties 수정

--- src/main/java/tripby/dao/MemberDao.java
+++ src/main/java/tripby/dao/MemberDao.java
@@ -1,5 +1,12 @@
 package tripby.dao;
 
-public interface MemberDao {
+import java.util.List;
+import tripby.domain.Member;
 
+public interface MemberDao {
+  public void insert(Member member);
+  public void update(int no);
+  public void delete(int no);
+  public List<Member> findAll();
+  public Member findBy(int no);
 }
--- src/main/java/tripby/domain/Member.java
+++ src/main/java/tripby/domain/Member.java
@@ -1,5 +1,17 @@
 package tripby.domain;
 
-public class Member {
+import lombok.Data;
 
+@Data
+public class Member {
+  private int memberNo;
+  private String email;
+  private String name;
+  private String password;
+  private String tel;
+  private String emailKey;
+  private String profilePhoto;
+  private int state;
+  private int grade;
+  private int restriction;
 }
