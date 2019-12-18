f610d202ab3b63196de2ea44c4307d5bf8499d67
Sohansaem <iuehakdrk@gmail.com>
2019-12-18 13:09:58 +0900

    회원가입 기능 시작

--- src/main/java/tripby/web/MemberController.java
+++ src/main/java/tripby/web/MemberController.java
@@ -1,5 +1,6 @@
 package tripby.web;
 
+import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import tripby.service.MemberService;
 @RequestMapping("/member")
 public class MemberController {
   
+  @Resource
   private MemberService memberService;
   
   @GetMapping("form")
@@ -21,7 +23,7 @@ public class MemberController {
   public String add(Member member) {
     System.out.println(member);
     memberService.insert(member);
-    return "redirect:member/form";
+    return "redirect:../member/form";
   }
   
   @GetMapping("list")
