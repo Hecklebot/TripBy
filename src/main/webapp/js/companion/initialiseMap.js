e7d94c4a1ef27d7c9600336e241b083818c14252
yimsb01 <yimsb02@naver.com>
2020-01-05 05:04:47 +0900

    페이지 UI 작업 중: 동행 찾기 + 동행 상세  + 동행 채팅 + 이전 동행 리스트

--- src/main/webapp/js/companion/initialiseMap.js
+++ src/main/webapp/js/companion/initialiseMap.js
@@ -1,7 +1,7 @@
-let container = document.getElementById('map');
-let options = {
+var container = document.getElementById('map');
+var options = {
   center: new kakao.maps.LatLng(33.450701, 126.570667),
   level: 3
 };
 
-let map = new kakao.maps.Map(container, options);
+var map = new kakao.maps.Map(container, options);
